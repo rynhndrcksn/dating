680a2efb4d152e0d4858e45c4d2c68d0b7bdfc6a
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-02-09 13:21:47 -0500

    added signup pages, can't align button (sanity rapidly deteriorating), and moved the navbar into the includes/ directory

--- index.php
+++ index.php
@@ -22,5 +22,26 @@ $f3->route('GET /', function() {
 	echo $view->render('views/home.html');
 });
 
+// start our signup routes (1/3)
+$f3->route('GET /sign-up-1', function() {
+	// create a new view, then sends it to the client
+	$view = new Template();
+	echo $view->render('views/sign-up-1.html');
+});
+
+// continue signup routes (2/3)
+$f3->route('POST /sign-up-2', function() {
+	// create a new view, then sends it to the client
+	$view = new Template();
+	echo $view->render('views/sign-up-2.html');
+});
+
+// end of our signup routes (3/3)
+$f3->route('POST /sign-up-3', function() {
+	// create a new view, then sends it to the client
+	$view = new Template();
+	echo $view->render('views/sign-up-3.html');
+});
+
 // run fat free HAS TO BE THE LAST THING IN FILE
 $f3->run();
