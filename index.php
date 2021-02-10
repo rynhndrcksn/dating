e6f4029e48b209c546f3f18b9ea5d49ff96e59a4
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-02-10 11:26:28 -0500

    outlined summary.html to prep it for next step. Gathered user supplied information in index.php and assigned to session variable

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// create a session
+session_start();
+
 // require autoload file
 require_once ('vendor/autoload.php');
 require_once ('model/data-layer.php');
@@ -24,7 +27,7 @@ $f3->route('GET /', function() {
 });
 
 // start our signup routes (1/3)
-$f3->route('GET /sign-up-1', function($f3) {
+$f3->route('GET|POST /sign-up-1', function($f3) {
 	// set the gender radio buttons
 	$f3->set('gens', getGens());
 
@@ -39,6 +42,24 @@ $f3->route('POST /sign-up-2', function($f3) {
 	$f3->set('gens', getGens());
 	$f3->set('states', getStates());
 
+	// gather user supplied information
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (isset($_POST['fname'])) {
+			$_SESSION['fname'] = $_POST['fname'];
+		}
+		if (isset($_POST['lname'])) {
+			$_SESSION['lname'] = $_POST['lname'];
+		}
+		if (isset($_POST['age'])) {
+			$_SESSION['age'] = $_POST['age'];
+		}
+		if (isset($_POST['gender'])) {
+			$_SESSION['gender'] = $_POST['gender'];
+		}
+		if (isset($_POST['phone'])) {
+			$_SESSION['phone'] = $_POST['phone'];
+		}
+	}
 	// create a new view, then sends it to the client
 	$view = new Template();
 	echo $view->render('views/sign-up-2.html');
@@ -50,9 +71,41 @@ $f3->route('POST /sign-up-3', function($f3) {
 	$f3->set('indoors', getInDoor());
 	$f3->set('outdoors', getOutDoor());
 
+	// gather user supplied information
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (isset($_POST['email'])) {
+			$_SESSION['email'] = $_POST['email'];
+		}
+		if (isset($_POST['state'])) {
+			$_SESSION['state'] = $_POST['state'];
+		}
+		if (isset($_POST['seeking'])) {
+			$_SESSION['seeking'] = $_POST['seeking'];
+		}
+		if (isset($_POST['biography'])) {
+			$_SESSION['biography'] = $_POST['biography'];
+		}
+	}
+
 	// create a new view, then sends it to the client
 	$view = new Template();
 	echo $view->render('views/sign-up-3.html');
+});
+
+// sign up summary route
+$f3->route('POST /summary', function() {
+	// gather user supplied information
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (isset($_POST['indoorInterests'])) {
+			$_SESSION['indoorInterests'] = $_POST['indoorInterests'];
+		}
+		if (isset($_POST['outdoorInterests'])) {
+			$_SESSION['outdoorInterests'] = $_POST['outdoorInterests'];
+		}
+	}
+	
+	$view = new Template();
+	echo $view->render('views/summary.html');
 });
 
 // run fat free HAS TO BE THE LAST THING IN FILE
